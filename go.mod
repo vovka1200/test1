3805334660593319258eccebced99fe2ee2ca57f
Vladimir Yavorskiy <vovka@krevedko.su>
2021-11-13 23:05:37 +0300

    init

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/vovka1200/test1
+
+go 1.16
+
+require (
+	github.com/sirupsen/logrus v1.8.1
+	google.golang.org/grpc v1.42.0
+	google.golang.org/protobuf v1.27.1
+)
---
+++ test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	log "github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+	"net"
+)
+
+type testService struct {
+	UnimplementedTestServiceServer
+}
+
+func (testservice *testService) Test(ctx context.Context, req *TestRequest) (*TestReply, error) {
+	log.WithFields(log.Fields{
+		"name": req.Inn,
+	}).Info("Request")
+	return &TestReply{Inn: "Test"}, nil
+}
+
+func main() {
+	if tcpServer, err := net.Listen("tcp", "localhost:8080"); err == nil {
+		log.WithFields(log.Fields{
+			"addr": tcpServer.Addr(),
+		}).Info("tcpServer")
+		rpcServer := grpc.NewServer()
+		RegisterTestServiceServer(rpcServer, &testService{})
+		if err := rpcServer.Serve(tcpServer); err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		log.Fatal(err)
+	}
+
+}
