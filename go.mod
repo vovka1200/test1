1a1167dee774064a45b7750b20c3ca5d3f1698cf
Vladimir Yavorskiy <vovka@krevedko.su>
2021-11-14 19:18:06 +0300

    grpc-gateway

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/vovka1200/test1
 go 1.16
 
 require (
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.6.0
 	github.com/sirupsen/logrus v1.8.1
 	google.golang.org/grpc v1.42.0
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.1.0 // indirect
 	google.golang.org/protobuf v1.27.1
 )
--- test.go
+++ test.go
@@ -2,20 +2,18 @@ package main
 
 import (
 	"context"
+	"fmt"
 	log "github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
+	"io/ioutil"
 	"net"
+	"net/http"
+	"regexp"
+	"strings"
 )
 
 type testService struct {
 	UnimplementedTestServiceServer
-}
-
-func (testservice *testService) Test(ctx context.Context, req *TestRequest) (*TestReply, error) {
-	log.WithFields(log.Fields{
-		"name": req.Inn,
-	}).Info("Request")
-	return &TestReply{Inn: "Test"}, nil
 }
 
 func main() {
@@ -31,5 +29,49 @@ func main() {
 	} else {
 		log.Fatal(err)
 	}
+}
 
+func (testservice *testService) Test(ctx context.Context, req *TestRequest) (*TestReply, error) {
+	url := fmt.Sprintf("https://www.rusprofile.ru/search?query=%s", req.Inn)
+	log.WithFields(log.Fields{
+		"name": req.Inn,
+		"url":  url,
+	}).Info("Request")
+	if resp, err := http.Get(url); err == nil {
+		if body, err := ioutil.ReadAll(resp.Body); err == nil {
+			buf := string(body)
+			inn := findField(buf, "id=\"clip_inn\">(.*)<")
+			title := findField(buf, "legalName\">(.*)<")
+			kpp := findField(buf, "id=\"clip_kpp\">(.*)<")
+			director := findDirector(buf)
+			return &TestReply{
+				Inn:      inn,
+				Title:    title,
+				Kpp:      kpp,
+				Director: director,
+			}, nil
+		} else {
+			log.Error(err)
+			return nil, err
+		}
+	} else {
+		log.Error(err)
+		return nil, err
+	}
+}
+
+func findField(body string, pattern string) string {
+	regex := regexp.MustCompile(pattern)
+	groups := regex.FindAllStringSubmatch(body, -1)
+	if len(groups) > 0 {
+		return groups[0][1]
+	}
+	return ""
+}
+
+func findDirector(body string) string {
+	i := strings.Index(body, "<span class=\"chief-title\">Генеральный директор</span>")
+	temp := []rune(body)
+	j := strings.Index(string(temp[i:]), "<div>")
+	return string(j)
 }
